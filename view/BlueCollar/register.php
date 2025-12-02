58b04fa104546ae8b742fd74be255a6d03b12911
ZillaKong <nene.quayenortey@ashesi.edu.gh>
2025-12-02 20:28:20 +0000

    register fix

--- view/BlueCollar/register.php
+++ view/BlueCollar/register.php
@@ -68,7 +68,7 @@
     </div>
 
     <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
-    <script src="../js/register.js"></script>
+    <script src="../../js/register.js"></script>
 
 </body>
 
